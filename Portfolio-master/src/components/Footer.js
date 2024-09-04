fb14422c6e4cf5e956762f076bd1daf7741527df
GJ <joshi700@gmail.com>
2024-09-04 10:01:51 +0530

    Update Footer.js

--- Portfolio-master/src/components/Footer.js
+++ Portfolio-master/src/components/Footer.js
@@ -14,10 +14,10 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Gaurav Joshi</h3>
+          <h3>Designed by Gaurav</h3>
         </Col>
         <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} GJ</h3>
+          <h3>Copyright © {year} Gaurav</h3>
         </Col>
         <Col md="4" className="footer-body">
           <ul className="footer-icons">
