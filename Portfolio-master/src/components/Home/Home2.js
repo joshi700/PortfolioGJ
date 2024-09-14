4b0cf9d1f176d6378facec1e687df75d0f693217
GJ <joshi700@gmail.com>
2024-09-15 00:07:29 +0530

    Update Home2.js

--- Portfolio-master/src/components/Home/Home2.js
+++ Portfolio-master/src/components/Home/Home2.js
@@ -18,7 +18,7 @@ function Home2() {
         <Row>
           <Col md={8} className="home-about-description">
             <h1 style={{ fontSize: "2.6em" }}>
-              LET ME <span className="purple"> INTRODUCE </span> MYSELF
+              A Product Manager who loves to talk to customers
             </h1>
             <p className="home-about-body">
             I’m a results-driven product management professional with over 15 years of experience in the <b className="purple">Payments industry</b>. 
@@ -26,25 +26,21 @@ function Home2() {
               <br />
               Currently, I’m a Product Manager at <b className="purple">Mastercard</b>, focusing on enhancing integration experiences for Payment Gateway's API offerings. 
               <br />
-              We are building innovative AI search agent to improve user engagement.
               <br />
-              <br />
-              Throughout my career, I’ve consistently delivered impactful results, from leading the modernization of payment systems to launching successful digital products. 
+              I love getting into the nitty-gritty details of design and work with a world-class team to solve real problems. 
               <br />
               My expertise spans Product Development, API integration, business analysis, and project leadership, with a strong emphasis on transforming user experiences.
               <br />
               
-              <br />I am fluent in Programming languages like
-              <i>
-                <b className="purple"> Python, Javascript. </b>
-              </i>
+              <br />I am keen on converting strategies into digital solutions that will benefit as many users as possible.
+              
               <br />
               <br />
               My field of Interest's are building new &nbsp;
               <i>
                 <b className="purple">Products </b> in areas related to{" "}
                 <b className="purple">
-                  Artificial Intelligence.
+                  Generative Artificial Intelligence.
                 </b>
               </i>
               <br />
