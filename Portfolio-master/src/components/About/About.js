2bbff76244c50918f7c879d7d7117c01337ee51a
GJ <joshi700@gmail.com>
2024-09-17 18:06:01 +0530

    Update About.js

--- Portfolio-master/src/components/About/About.js
+++ Portfolio-master/src/components/About/About.js
@@ -21,7 +21,9 @@ function About() {
               paddingBottom: "50px",
             }}
           >
-
+              <h1 style={{ fontSize: "2.1em", paddingBottom: "20px" }}>
+              Know what <strong className="purple">I do</strong>
+            </h1>
             <Aboutcard />
           </Col>
           <Col
